db71ce6e067361f5f53d7ba58d6fbd14b9ea598a
Pedro Souto Duarte Junior <pedro.duartejunior@gmail.com>
2023-08-01 09:44:57 -0300

    finalização

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,7 +2,7 @@ import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard (titulo, descricao, url, imagem) {
+export default function constroiCard (titulo, descricao, url, imagem) {
   const video = document.createElement("li");
   video.className = "videos__item";
   video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
@@ -15,13 +15,18 @@ function constroiCard (titulo, descricao, url, imagem) {
   <p>${descricao}</p>
   </div>`
 
-return video; 
+  return video; 
 }
 
 async function listaVideos() {
+
+  try {
   const listaApi = await conectaApi.listaVideos();
   listaApi.forEach(elemento => lista.appendChild(
     constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+  } catch {
+    lista.innerHTML = `<h2 class="mensagem__titulo">Não foi possível exibir a lista de videos</h2>`
+  }
 }
 
 listaVideos();
